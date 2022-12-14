3e1ac04c974fc15481162486b50517a3cc1e31f6
Nikita <enduraew@gmail.com>
2022-12-14 22:14:17 +0500

    let trought to the course AL

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 'use strict';
 
-const a = 1;
+// Базовые арифметические операторы
+// const width = 10;
+// const height = 5;
+// const space = width * height;
+// const newWidth = width - 4;
+// const newWidth2 = width + 4;
+// const division = newWidth / newWidth2;
+// const volume = 2 ** 3; // 2 * 2 * 2
+// console.log(volume);
 
-console.log(a);
+// // Строки
+// const city = 'Москва';
+// const street = 'Новослободская';
+// console.log(city + ', ' + street + ' ' + 5);
 
-a = 'test';
-console.log(a);
+// Операторы присваивания
+
+/* '=' - это оператор присваивания */
+// let age = 25 + 5;
+// age += 2; // age = age + 2;
+// age -= 3;
+// age *= 2;
+// age /= 2;
+
+// age++; // age = age + 1'
+// console.log(age);
+
+// // Операторы сравнения
+// const nikita = 20;
+// console.log(age > nikita);
+// console.log(age >= nikita);
+// console.log(age < nikita);
+// console.log(age <= nikita);
+// console.log(age == nikita);
+
+// const isSuited = 100 - 10 > 90 - 5;
+// console.log(isSuited); // true
+
+// 100 - (12 приоритет) 10 > (10) 90 - (12) 5;
+
+// const a = (6 + 10) / 2;
+// console.log(a);
+
+// let b;
+// let c;
+// c = b = 100 + 50;
+// console.log(c);
+// console.log(b);
