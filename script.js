cadc9645e78043f94a2fdfa386ea571d5d098143
Nikita <enduraew@gmail.com>
2022-12-17 22:25:38 +0500

    learn js

--- script.js
+++ script.js
@@ -48,3 +48,46 @@
 // c = b = 100 + 50;
 // console.log(c);
 // console.log(b);
+
+// let a = 5;
+// let b = 5.6;
+// console.log(typeof b);
+// a = 'строка';
+// console.log(typeof a);
+// let isAdmin = a > 10;
+// console.log(typeof isAdmin);
+
+// let c;
+// console.log(typeof c);
+// c = 5;
+// console.log(typeof c);
+
+// let d = null;
+// console.log(d === null);
+
+/* 
+
+УРОК №16. Practice 
+
+Ваша часовая ставка 80$ и вы готовы работать не
+более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40
+часов работы.
+Сейчас понедельник.
+Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
+
+*/
+
+/*
+// данные
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+
+// результат
+console.log('Смогу ли я работать ' + (availableHours > projectHours));
+console.log('Стоимость моей работы: ' + payRateUSD * projectHours);
+*/
